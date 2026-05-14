579e287e2a35d67577c9bb5bc23310650b28f73b
Junior Antunes <jugabriel2612@gmail.com>
2026-05-14 14:29:46 -0300

    Feat: Script

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let temaBtn = document.getElementById('mudar-tema');
 let temaAtual = "claro";
 let body = document.body;
+let icone = temaBtn.querySelector('svg');
+let card = document.getElementsByTagName('card-box');
 
-temaBtn.onclick = function(){
-    if(temaAtual === "claro"){
+temaBtn.onclick = function() {
+    if (temaAtual === "claro") {
         temaAtual = "escuro";
-        body.style.background = "#1E1E1E";
-    }else{
-        temaAtual = "claro"
-        body.style.background = "#F9F9F9";
+        body.style.backgroundColor = "#1E1E1E";
+        icone.style.stroke = "white"; 
+        body.style.color = "white";
+
+    } else {
+        temaAtual = "claro";
+        body.style.backgroundColor = "white";
+        icone.style.stroke = "black"; 
+        card.style.backgroundColor = "#2C2C2C";
+        card.style.color = "white";
     }
 }
